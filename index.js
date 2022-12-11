e216f052b30f71a6ca6173da09ca9c60c26a0a85
royal1er <Odiath000!>
2022-12-10 21:42:38 -0400

    Ajout de la fonctionnalité d'ajout dans la listes des livres à lire Enregistrement du livre dans une session, en utilisant l’API SessionStorage. Ajout de la suppression d’un livre de la liste

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var showform = document.getElementById('showform');
 var btnMod = document.getElementById('openshowform');
 var booklist = document.getElementById('booklist');
+var searchmybook = 1;
+var findmybook = 1;
+//sessionStorage.clear();
 
 /*=========================================*/
 const openmodalbutton = document.getElementById("openshowform")
@@ -37,6 +40,7 @@ async function getapi(url) {
     
     // Storing data in form of JSON
     var data = await response.json();
+    this.mydata = data;
     console.log(data);
     if (response) {
         showBookList();
@@ -49,19 +53,16 @@ async function getapi(url) {
 function showBookList() {
     document.getElementById('booklist').style.display = 'grid';
 }
-// Function to define innerHTML for HTML table
 async function show(data) {
-    let titlebook = document.getElementById("booklist-title");
-    let idbook = document.getElementById("book-id");
-    let bookresume = document.getElementById("book-desc-resume");
     let testmybook = document.getElementById("bookdesc");
-    // Loop to access all rows 
+    let i = 0;
     for (let r of data.items) {
+      
       testmybook.innerHTML += `
       <div id="mybookdesc">
       <div id="booklist-title">Titre du livre : ${r.volumeInfo.title}</div>
       <div id="bookmark">
-      <img src="images/bookmark.svg" alt="">
+      <img class="test" src="images/bookmark.svg" alt="${i}">
       </div>
       <div class="book-desc-author">
           Auteur du livre : ${r.volumeInfo.authors}
@@ -80,6 +81,101 @@ async function show(data) {
         : `${r.volumeInfo.imageLinks.thumbnail}`}" alt="">
       </div>
   </div>`;
+  i++;
     }
+    getTheBook(data);
+}
+
+/**/
+
+function getTheLastNumber(){
+  var n = 0;
+  for (let i = 1; i < sessionStorage.length; i++) {
+    var a = sessionStorage.key(i);
+    if(a > n){
+      n = a;
+    }
+    //console.log(i);
+    console.log(sessionStorage);
+  }
+  return n;
+}
+console.log(getTheLastNumber());
+
+
+
+if(sessionStorage.length >= 1){
+  showMyBook();
+}
+
+async function showMyBook() {
+  let testmybook = document.getElementById("showbookmark");
+  for (let i = 1; i < sessionStorage.length; i++) {
+
+    while(sessionStorage.getItem(searchmybook) == null){
+      searchmybook++
+    }
+    data = sessionStorage.getItem(searchmybook);
+    data = JSON.parse(data);
+    testmybook.innerHTML += `
+    <div id="mybookdesc">
+    <div id="booklist-title">Titre du livre : ${data.volumeInfo.title}</div>
+    <div id="bookmark">
+    <img class="delete" src="images/trash.svg" alt="${searchmybook}">
+    </div>
+    <div class="book-desc-author">
+        Auteur du livre : ${data.volumeInfo.authors}
+        </div>
+    <div id="book-id">
+       Identifiant du livre :  ${data.id}
+    </div>
+    <div id="book-desc-resume">
+        Description : ${data.volumeInfo.description === undefined
+          ? "Information manquante"
+          : `${data.volumeInfo.description}`}
+    </div>
+    <div id="book-desc-img">
+    <img src="${data.volumeInfo.imageLinks === undefined
+      ? "images/unavailable.png"
+      : `${data.volumeInfo.imageLinks.thumbnail}`}" alt="">
+    </div>
+</div>`;
+searchmybook++;
+  }
+
+}
+
+async function getTheBook(data) {
+  document.addEventListener("click", (e) => {
+    const button = e.target.closest(".test");
+    if (button) {
+      myalt = button.getAttribute("alt");
+      console.log(myalt);
+      finaldata=data.items[myalt];
+      console.log(finaldata);
+      i = getTheLastNumber();
+      console.log(i);
+      i++;
+      console.log(i);
+      sessionStorage.setItem(i.toString(),JSON.stringify(finaldata));
+    console.log(Object.keys(sessionStorage).length);
+    
+    }
+  })
+  
+}
+deleteTheBook();
+function deleteTheBook() {
+  document.addEventListener("click", (e) => {
+    const button = e.target.closest(".delete");
+    if (button) {
+      myalt = button.getAttribute("alt");
+      indexOfBook = myalt.toString();
+      console.log(indexOfBook);
+      sessionStorage.removeItem(indexOfBook);
+      window.location.reload();
+    }
+  })
+  
 }
 
